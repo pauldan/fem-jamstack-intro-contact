66b142c7c0f05d13302d01ed868defea109ad210
Paul Dan <paul.dan@gmail.com>
2020-07-31 07:13:40 +0300

    fixed contact function

--- functions/contact.js
+++ functions/contact.js
@@ -1,8 +1,14 @@
 const { transport, emailTemplate } = require('../utils/mail');
 exports.handler = async (event, _context, callback) => {
-  console.log(emailTemplate(event.body));
+  const email = JSON.parse(event.body);
+  console.log(emailTemplate(email));
   try {
-    const res = await transport.sendMail(emailTemplate(event.body));
+    const res = await transport.sendMail({
+      from: 'contact@form.abc',
+      to: 'email@form.abc',
+      subject: email.subject,
+      html: emailTemplate(body),
+    });
     callback(null, {
       statusCode: 200,
       body: JSON.stringify({ sent: true }),
